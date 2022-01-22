9f696af5089c3f34cc45a5c36ed72b456b60df14
834930269 <834930269@qq.com>
2022-01-22 16:33:44 +0800

    [feature] 集成Glad(Opengl接口)

--- GameEngine/Hazel/src/Platform/Windows/WindowsWindow.cpp
+++ GameEngine/Hazel/src/Platform/Windows/WindowsWindow.cpp
@@ -4,6 +4,8 @@
 #include "Hazel/Events/KeyEvent.h"
 #include "Hazel/Events/MouseEvent.h"
 
+#include <Glad/glad.h>
+
 namespace Hazel {
 	static bool s_GLFWInitialized = false;
 
@@ -43,6 +45,11 @@ namespace Hazel {
 		}
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
 		glfwMakeContextCurrent(m_Window);
+
+		//把glfw的glfwGetProcAddress函数赋给glad(使用函数指针转型)
+		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+		HZ_CORE_ASSERT(status, "无法初始化Glad!");
+
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
 
