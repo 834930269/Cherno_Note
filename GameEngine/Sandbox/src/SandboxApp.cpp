87e68d548acaa281b0bb6a091b42e4b05e86a743
834930269 <834930269@qq.com>
2022-01-24 15:09:00 +0800

    [feature] 输入事件

--- GameEngine/Sandbox/src/SandboxApp.cpp
+++ GameEngine/Sandbox/src/SandboxApp.cpp
@@ -4,10 +4,15 @@ class ExampleLayer :public Hazel::Layer {
 public:
 	ExampleLayer() : Layer("Example"){}
 	void OnUpdate() override {
-		HZ_INFO("ExampleLayer::Update");
+
 	}
 	void OnEvent(Hazel::Event& event) override {
-		HZ_TRACE("{0}", event);
+		if (event.GetEventType() == Hazel::EventType::KeyPressed) {
+			Hazel::KeyPressedEvent& e = (Hazel::KeyPressedEvent&)event;
+			if(e.GetKeyCode()==HZ_KEY_TAB)
+				HZ_TRACE("Tab is pressed!");
+			HZ_TRACE("{0}", (char)e.GetKeyCode());
+		}
 	}
 };
 
