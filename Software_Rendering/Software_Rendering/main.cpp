af311790dbe9251ff7a823aee896a2a26d8c53e0
834930269 <834930269@qq.com>
2022-03-26 20:31:21 +0800

    [feature] uv coord / shader / renderer pipeline

--- Software_Rendering/Software_Rendering/main.cpp
+++ Software_Rendering/Software_Rendering/main.cpp
@@ -1,132 +1,73 @@
 ﻿#include <vector>
-#include <cmath>
-#include <cstdlib>
-#include <limits>
+#include <iostream>
+
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
+#include "our_gl.h"
 
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
-const TGAColor green = TGAColor(0, 255, 0, 0);
 Model* model = NULL;
-TGAImage *img = NULL;
 const int width = 800;
 const int height = 800;
 
-void line(Vec2i x0, Vec2i x1, TGAImage& image, TGAColor color) {
-    bool steep = false;
-    if (std::abs(x0.x - x1.x) < std::abs(x0.y - x1.y)) { // if the line is steep, we transpose the image 
-        std::swap(x0.x, x0.y);
-        std::swap(x1.x, x1.y);
-        steep = true;
-    }
-    if (x0.x > x1.x) { // make it left−to−right 
-        std::swap(x0.x, x1.x);
-        std::swap(x0.y, x1.y);
-    }
+Vec3f light_dir(1, 1, 1);
+Vec3f eye(0, -1, 3);
+Vec3f center(0, 0, 0);
+Vec3f up(0, 1, 0);
 
-    int dx = x1.x - x0.x;
-    int dx2 = dx * 2;
-    int dy = x1.y - x0.y;
-    int derror = std::abs(dy)*2;//每次增加多少
-    int error = 0;    //累计步进
-    int y = x0.y;         //初始步进
-    for (int x = x0.x; x <= x1.x; x++) {
-        if (steep) {
-            image.set(y, x, color);
-        }
-        else {
-            image.set(x, y, color);
-        }
-        error += derror;    //累加步进
-        if (error > dx) {   //如果步进大于.5,更新y
-            y += (x1.y > x0.y ? 1 : -1);
-            error -= dx2;
-        }
-    }
-}
+struct GouraudShader : public IShader {
+	Vec3f varying_intensity;	//通过顶点shader写入,由片元着色器读取
+	mat<2, 3, float> varying_uv;  // same as above
+	//模型的第i个三角形,第n个顶点
+	virtual Vec4f vertex(int iface, int nthvert) {
+		//设置设置三个点的uv
+		varying_uv.set_col(nthvert, model->uv(iface, nthvert));
+		Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert));	//顶点
+		gl_Vertex = Viewport * Projection * ModelView * gl_Vertex;//顶点转到屏幕空间
+		varying_intensity[nthvert] = std::max(0.f, model->normal(iface, nthvert) * light_dir);
+		return gl_Vertex;
+	}
 
-//求重心坐标,目的是为了求Z-buffer
-Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) {
-    Vec3f s[2];
-    for (int i = 2;i--;) {
-        s[i][0] = C[i] - A[i];
-        s[i][1] = B[i] - A[i];
-        s[i][2] = A[i] - P[i];
-    }
+	//bar是重心坐标,color是颜色
+	virtual bool fragment(Vec3f bar, TGAColor& color) {
+		float intensity = varying_intensity * bar;	//得到每个片元的
+		Vec2f uv = varying_uv * bar;	//计算每个片元的uv
+		color = model->diffuse(uv) * intensity;
+		return false;	//不剪切
+	}
+};
 
-    Vec3f u = cross(s[0], s[1]);
-    if (std::abs(u[2]) > 1e-2) {
-        return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
-    }
-    return Vec3f(-1, 1, 1);
-}
+int main(int argc,char **argv) {
+	if (2 == argc) {
+		model = new Model(argv[1]);
+	}
+	else {
+		model = new Model("obj/african_head.obj");
+	}
+	//预计算MVP矩阵
+	lookat(eye, center, up);
+	viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
+	projection(-1.f / (eye - center).norm());
+	light_dir.normalize();
 
-void triangle(Vec3f *pts,float *zbuffer, TGAImage& image, TGAColor color) {
-    Vec2f bboxmin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
-    Vec2f bboxmax(std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
-    Vec2f clamp(image.get_width() - 1, image.get_height() - 1);
-    //算出三角形的边界范围
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 2; j++) {
-            bboxmin[j]=std::max(0.f,std::min(bboxmin[j],pts[i][j]));
-            bboxmax[j]=std::min(clamp[j],std::max(bboxmax[j],pts[i][j]));
-        }
-    }
-    Vec3f P;
-    for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
-        for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
-            //求重心坐标
-            Vec3f bc_screen = barycentric(pts[0], pts[1], pts[2],P);
-            //如果不在三角形内,就继续
-            if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
-            P.z = 0;
-            //根据重心坐标求当前像素的z坐标,bc_screen是三个顶点的权重
-            //把每个顶点上的z坐标按这个权重加起来就是z坐标
-            for (int i = 0; i < 3; i++) P.z += pts[i][2] * bc_screen[i];
-            //如果原来的z坐标小于新的z坐标,则更新
-            if (zbuffer[int(P.x + P.y * width)] < P.z) {
-                zbuffer[int(P.x + P.y * width)] = P.z;
-                //如果比缓存的z坐标要小,，就不必在画了
-                image.set(P.x, P.y, color);
-            }
-        }
-    }
+	TGAImage image(width, height, TGAImage::RGB);
+	TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
 
-}
+	GouraudShader shader;
+	for (int i = 0; i < model->nfaces(); i++) {
+		Vec4f screen_coords[3];
+		for (int j = 0; j < 3; j++) {
+			screen_coords[j] = shader.vertex(i, j);
+		}
+		triangle(screen_coords, shader, image, zbuffer);
+	}
 
-Vec3f world2screen(Vec3f v) {
-    return Vec3f(int((v.x + 1.) * width / 2. + .5), int((v.y + 1.) * height / 2. + .5), v.z);
-}
-
-int main(int argc, char** argv) {
-    if (2 == argc) {
-        model = new Model(argv[1]);
-    }
-    else {
-        model = new Model("obj/african_head.obj");
-        img = new TGAImage(width, height, TGAImage::RGB);
-    }
-    img->read_tga_file("obj/african_head_diffuse.tga");
-
-    float* zbuffer = new float[width * height];
-    //初始化为最小值
-    for (int i = width * height; i--; zbuffer[i] = -std::numeric_limits<float>::max());
-
-    TGAImage image(width, height, TGAImage::RGB);
-    for (int i = 0; i < model->nfaces(); i++) {
-        std::vector<int> face = model->face(i);
-        Vec3f pts[3];
-        for (int i = 0; i < 3; i++)pts[i] = world2screen(model->vert(face[i]));
-        triangle(pts, zbuffer, image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
-    }
-
-    image.flip_vertically();	//希望原点在左下方
+	image.flip_vertically(); // to place the origin in the bottom left corner of the image
+	zbuffer.flip_vertically();
 	image.write_tga_file("output.tga");
+	zbuffer.write_tga_file("zbuffer.tga");
 
-    //释放model
-    delete model;
-    delete[]zbuffer;
+	delete model;
+
 	return 0;
 }
